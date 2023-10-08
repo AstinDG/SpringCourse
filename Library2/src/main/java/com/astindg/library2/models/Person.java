352847755746992dd4a8389b0ef0f708a7fe5aa3
AstinDG <astindg@gmail.com>
2023-10-08 15:28:50 +0300

    Refactoring Person entity field "personId"

--- Library2/src/main/java/com/astindg/library2/models/Person.java
+++ Library2/src/main/java/com/astindg/library2/models/Person.java
@@ -14,7 +14,7 @@ public class Person {
     @Id
     @Column(name = "person_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int person_id;
+    private int personId;
 
     @Column(name = "name")
     @NotEmpty(message = "Name should not be empty")
@@ -40,12 +40,12 @@ public class Person {
         this.yearOfBirth = age;
     }
 
-    public int getPerson_id() {
-        return person_id;
+    public int getPersonId() {
+        return personId;
     }
 
-    public void setPerson_id(int person_id) {
-        this.person_id = person_id;
+    public void setPersonId(int personId) {
+        this.personId = personId;
     }
 
     public String getName() {
@@ -75,7 +75,7 @@ public class Person {
     @Override
     public String toString() {
         return "Person{" +
-                "person_id=" + person_id +
+                "person_id=" + personId +
                 ", name='" + name + '\'' +
                 ", yearOfBirth=" + yearOfBirth +
                 '}';
